a07f415af43d45480781e43174655ca2492b7546
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 19:44:26 +1300

    Add student and course model

---
+++ EFC_Student/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFC_Student.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ EFC_Student/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFC_Student.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        ICollection<Course> Courses { get; set; }
+    }
+}
