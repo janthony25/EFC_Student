83f9a9beb458dc6f336bf175aeba8390f6b6e6d9
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 19:49:40 +1300

    Add ApplicationDbContext

---
+++ EFC_Student/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EFC_Student.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFC_Student.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .HasOne<Student>(i => i.Student)
+                .WithMany(c => Courses)
+                .HasForeignKey(i => i.StudentId)
+                .IsRequired();
+        }
+    }
+}
